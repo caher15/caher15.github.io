97ab4cca7ae0afd2d212f6474474d40f9b68989e
caher15 <140141431+caher15@users.noreply.github.com>
2024-05-04 13:05:57 -0600

    First commit

---
+++ base-de.preguntas.AD.js
@@ -0,0 +1,35 @@
+let basePreguntas = [
+    //Pregunta 1
+    {
+        pregunta: "La siguiente imagen es la representación de una señal...",
+        ayuda: "Los sonidos que escuchamos en la música, la radio, la televisión y las conversaciones telefónicas se transmiten en forma de esta señal",
+        imagen: "./img-act_AD/Analoga.png",
+        respuesta: "Análoga",
+        distractores:["Digital", "Radio", "Oblicua"],
+    
+        },
+    //Pregunta 2
+     //Pregunta 2
+     {
+        pregunta: "Las señales digitales támbien son conocidas como...",
+        respuesta: "Discretas",
+        distractores:["Continuas", "Electrónicas", "Audio"],
+    
+        },
+        //Pregunta 3
+    {
+        pregunta: "Calor , Velocidad y Humedad son ejemplos de variables...", 
+        ayuda: "Es or",
+        respuesta: "analógicas",
+        distractores:["digitales", "", ""],
+    
+        },
+    //Pregunta 4
+    {
+        pregunta: "Son los parámetros de una señal digital", 
+        respuesta: "Altura, Duración, Frecuencia",
+        distractores:["Altura, Frecuencia, Peso", "Base, Altura, Velocidad", "Tiempo, Valor, Velocidad"],
+    
+        },
+    
+];
